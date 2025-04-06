8a2b7f41de717d7ea8268fa19b03d2a02b4012af
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-04-06 10:10:05 +0200

    Implemented Endpoint /add_books: POST request renders a form, queries the database to add author data and returns a message informing the user of the resulting operation.

--- app.py
+++ app.py
@@ -1,11 +1,98 @@
-from flask import Flask
+"""
+This is a simple Flask application that uses SQLAlchemy to
+manage a library database.
+This module sets up the Flask application and SQLAlchemy
+database connection.
+"""
+
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book
+import webbrowser
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/veronica/PycharmProjects/Book-Alchemy/data/library.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:////Users/veronica/PycharmProjects/Book-Alchemy/data/library.sqlite'
 db.init_app(app)
 
-# Create the database and tables
-with app.app_context():
-    db.create_all()
+## Create the database and tables
+#with app.app_context():
+#    db.create_all()
+
+
+## API endpoints
+
+@app.route('/')
+def get_books():
+    """
+    Objectives:
+    Create a Flask route for the home page that queries the
+    Book table and returns:
+    - The render_template() function for the home.html template
+    - The books data returned by the database query, in a format
+    the Jinja code in the HTML file expects.
+    """
+    ## Query the database for all books
+    books = Book.query.all()
+    ## Create a list of dictionaries to store book data
+    books_data = []
+    for book in books:
+        book_data = {
+            'id': book.id,
+            'isbn': book.isbn,
+            'title': book.title,
+            'publication_date': book.publication_date,
+            'author_id': book.author_id
+        }
+        books_data.append(book_data)
+    ## Render the template with the books data
+    return render_template('home.html'), 200
+
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    """
+    Adds a new author to the database.
+
+    If a Get request is made, it returns a render of the
+    add_author.html template.
+
+    If a Post request is made, it retrieves the data from the
+    form, checks if the author already exists in the database,
+    and if not, adds the new author to the database.
+    If the author already exists, it returns an error message.
+    """
+    if request.method == 'POST':
+        ## Get the data from the form
+        new_author = Author(
+            name=request.form.get('name'),
+            birth_date=request.form.get('birth_date'),
+            date_of_death=request.form.get('date_of_death')
+        )
+
+        ## Check if the author already exists in the database
+        existing_author = Author.query.filter_by(name=new_author.name).first()
+        if existing_author:
+            ## If the author already exists, show an error message
+            message = f"Author '{new_author.name}' already exists."
+            return render_template('add_author.html', message=message)
+
+        ## Add the new author to the database
+        db.session.add(new_author)
+        db.session.commit()
+        ## Show a success message
+        message = f"Author '{new_author.name}' added successfully."
+        return render_template('add_author.html', message=message)
+
+    ## If the request method is GET, render the form
+    return render_template('add_author.html')
+
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    pass
+
+
+if __name__ == '__main__':
+    webbrowser.open_new('http://127.0.0.1:5000/add_author')
+    app.run(debug=True)
--- data_models.py
+++ data_models.py
@@ -1,10 +1,33 @@
+"""
+This module defines the data models for the application
+using SQLAlchemy.
+
+* db is an instance of SQLAlchemy that is used to interact
+with the database.
+
+* Author and Book classes, which represent authors and books
+tables in the database, respectively.
+"""
+
 from sqlalchemy import Integer, String, ForeignKey
 from flask_sqlalchemy import SQLAlchemy
 
 # Flask-SQLAlchemy instance
 db = SQLAlchemy()
 
+
 class Author(db.Model):
+    """
+    Represents an author in the database.
+    Attributes:
+        id (int): The unique identifier for the author.
+        name (str): The name of the author.
+        birth_date (str): The birthdate of the author.
+        date_of_death (str): The date of death of the author.
+        books (list): A list of books written by the author.
+    Methods:
+        __repr__(): Returns a string representation of the author.
+    """
     __tablename__ = 'authors'
     id = db.Column(Integer, primary_key=True, autoincrement=True)
     name = db.Column(String)
@@ -17,7 +40,18 @@ class Author(db.Model):
                 f"birth_date='{self.birth_date}', "
                 f"date_of_death='{self.date_of_death}')")
 
+
 class Book(db.Model):
+    """
+    Represents a book in the database.
+    Attributes:
+        id (int): The unique identifier for the book.
+        isbn (int): The ISBN-number of the book.
+        title (str): The title of the book.
+        publication_date (str): The publication date of the book.
+        author_id (int): The ID of the author of the book.
+        author (Author): The author of the book.
+    """
     __tablename__ = 'books'
     id = db.Column(Integer, primary_key=True, autoincrement=True)
     isbn = db.Column(Integer)
